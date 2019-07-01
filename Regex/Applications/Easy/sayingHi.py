9d35ece7244b4a7ee735405625bdd4b43934666b
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:28:14 -0300

    Update sayingHi.py

--- Regex/Applications/Easy/sayingHi.py
+++ Regex/Applications/Easy/sayingHi.py
@@ -1,11 +1,14 @@
 import re
 import sys
 
+#greeting pattern
 Regex_Pattern = r'^[Hh][Ii][\s][^Dd]'
 
+#retrive N
 inputText = sys.stdin
 n = int(inputText.readline())
 
+#for each input, print if matches greeting
 while n > 0:
     Test_String = inputText.readline()
     if ( len(re.findall(Regex_Pattern, Test_String)) > 0 ):
