a20b30e10091a1b96466477e8690489fc64e443b
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:21:57 -0300

    Update findAWord.py

--- Regex/Applications/Medium/findAWord.py
+++ Regex/Applications/Medium/findAWord.py
@@ -1,28 +1,26 @@
 import re
 import sys
 
+#retrive N
 inputText = sys.stdin
 n = int(inputText.readline())
 lines = []
 
+#retrive lines
 while n > 0:
     lines.append(inputText.readline())
     n = n - 1
 
-#print lines
-
+#retrive T
 t = int(inputText.readline())
 
+#for each word
 while t > 0:
     match = []
-    word = inputText.readline()
-    word = word.replace("\n", "")
-    #print "Word:", word
-    Regex_Pattern = r'(^|(?<=[\W]))'+ re.escape(word) + '((?=[\W])|$)'
-    #print "RegEx:", Regex_Pattern
+    word = inputText.readline()         #retrive word
+    word = word.replace("\n", "")               #remove \n char
+    Regex_Pattern = r'(^|(?<=[\W]))'+ re.escape(word) + '((?=[\W])|$)'              #update regEx
     for line in lines:
-        #print "re.FA:", re.findall(Regex_Pattern, line)
-        match.extend(re.findall(Regex_Pattern, line))
-    #print "Match:", match
-    print len(match)
+        match.extend(re.findall(Regex_Pattern, line))       #for each line, find matches
+    print len(match)            #print match frequency
     t = t - 1
