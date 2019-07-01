279b6e50b61076c7370a88bf7fdd8fd0795f4dc7
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:27:17 -0300

    Update languageDetect.py

--- Regex/Applications/Easy/languageDetect.py
+++ Regex/Applications/Easy/languageDetect.py
@@ -1,15 +1,19 @@
-Regex_Pattern = r'[A-Z]+'    # Do not delete 'r'.
+#word pattern
+Regex_Pattern = r'[A-Z]+'
 
 import re
 import sys
 
+#supported languages list
 strLangs = "C:CPP:JAVA:PYTHON:PERL:PHP:RUBY:CSHARP:HASKELL:CLOJURE:BASH:SCALA: ERLANG:CLISP:LUA:BRAINFUCK:JAVASCRIPT:GO:D:OCAML:R:PASCAL:SBCL:DART: GROOVY:OBJECTIVEC"
 languages = []
 languages.extend(re.findall(Regex_Pattern, strLangs))
 
+#retrive N
 inputText = sys.stdin
 n = int(inputText.readline())
 
+#for each input, validate if language is supported
 while n > 0:
     match = []
     Test_String = inputText.readline()
