a44b938c90ba697a5261eed5e7fa3baba7feca55
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:24:23 -0300

    Update detectPAN.py

--- Regex/Applications/Easy/detectPAN.py
+++ Regex/Applications/Easy/detectPAN.py
@@ -1,11 +1,14 @@
 import re
 import sys
 
+#PAN pattern
 Regex_Pattern = r'^[A-Z]{5}[0-9]{4}[A-Z]{1}$'    # Do not delete 'r'.
 
+#retrive N
 inputText = sys.stdin
 n = int(inputText.readline())
 
+#for each case, find PAN matches
 while n > 0:
     match = []
     Test_String = inputText.readline()
