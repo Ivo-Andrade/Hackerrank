5ef88edfbfca9c12b9a137fb8272aa6b350d84e4
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 14:03:18 -0300

    Update programmingLanguageDetection.py

--- Regex/Applications/Medium/programmingLanguageDetection.py
+++ Regex/Applications/Medium/programmingLanguageDetection.py
@@ -1,17 +1,18 @@
 import re
 import sys
 
-#Java pattern
-regexJava = r'import'
+#Non-Python pattern
+regexNotPython = r';$'
 
 #C pattern
 regexC = r'#include'
 
 #from input, detect language
 inputText = sys.stdin.read()
-if re.search(regexJava,inputText):
-    print "Java"
-elif re.search(regexC,inputText):
-    print "C"
+if re.search(regexNotPython,inputText):
+    if re.search(regexC,inputText):
+        print "C"
+    else:
+        print "Java"
 else:
     print "Python"
