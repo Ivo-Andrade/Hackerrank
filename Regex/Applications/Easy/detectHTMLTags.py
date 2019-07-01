9b5cb72f008b34dd9204397bb0c971ab83058944
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:48:21 -0300

    Create detectHTMLTags.py

---
+++ Regex/Applications/Easy/detectHTMLTags.py
@@ -0,0 +1,31 @@
+import re
+import sys
+
+#opening tag pattern
+Regex_Pattern = r'<\s*([^/][a-zA-Z0-9]*)'
+
+#retrive N
+inputText = sys.stdin
+n = int(inputText.readline())
+lines = []
+
+#retrive lines
+while n > 0:
+    lines.append(inputText.readline())
+    n = n - 1
+
+#for each line, verify matches for tags
+match = []
+for line in lines:
+    match.extend(re.findall(Regex_Pattern, line))
+    
+#remove duplicates, sort list
+match = list(dict.fromkeys(match))
+match.sort()
+
+#print tags
+for tag in match[:-1]:
+    sys.stdout.write(tag + ";")
+
+for tag in match[-1:]:
+    sys.stdout.write(tag)
