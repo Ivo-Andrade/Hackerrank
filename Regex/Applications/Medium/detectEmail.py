39f060690047133e68fdfbb12cf7a174fdbf31ff
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:23:28 -0300

    Update detectEmail.py

--- Regex/Applications/Medium/detectEmail.py
+++ Regex/Applications/Medium/detectEmail.py
@@ -1,3 +1,4 @@
+#Email pattern
 Regex_Pattern = r'[\w+\.]*[\w]@[\w+\.]*[\w]'    # Do not delete 'r'.
 
 import re
@@ -5,12 +6,15 @@ import sys
 
 match = []
 
+#from input, find Email matches
 for Test_String in sys.stdin:
     match.extend(re.findall(Regex_Pattern, Test_String))
 
+#remove duplicates, sort list
 match = list(dict.fromkeys(match))
 match.sort()
 
+#print Emails
 for email in match[:-1]:
     sys.stdout.write(email + ";")
 
