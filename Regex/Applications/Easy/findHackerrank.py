87d964f64fe339af81b3546f8b2ed5c35259f5cd
Ivo de Andrade <ivo_andradedeus@hotmail.com>
2019-07-01 10:25:08 -0300

    Update findHackerrank.py

--- Regex/Applications/Easy/findHackerrank.py
+++ Regex/Applications/Easy/findHackerrank.py
@@ -1,12 +1,15 @@
 import re
 import sys
 
+#hackerrank patterns
 Regex_Pattern_StartsWith = r'^hackerrank'
 Regex_Pattern_EndsWith = r'hackerrank$'
 
+#retrive N
 inputText = sys.stdin
 n = int(inputText.readline())
 
+#detect case
 while n > 0:
     Test_String = inputText.readline()
     if ( (len(re.findall(Regex_Pattern_StartsWith, Test_String)) > 0) & (len(re.findall(Regex_Pattern_EndsWith, Test_String)) > 0) ):
